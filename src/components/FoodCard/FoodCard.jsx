846925328fb5ad11e1375ebb61e2ec12d43ae588
imransarkerweb <imransarker.web@gmail.com>
2023-05-30 17:43:49 +0600

    design updated

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -8,11 +8,13 @@ const FoodCard = ({ item }) => {
       <p className="bg-neutral-900 absolute text-white right-0 mt-4 mr-4 px-4">
         ${price}
       </p>
-      <div className="card-body">
+      <div className="card-body flex flex-col items-center">
         <h2 className="card-title">{name}</h2>
         <p>{recipe}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Add to cart</button>
+          <button className="btn btn-outline border-0 border-b-4 mt-4 bg-slate-100 border-orange-500">
+            Add to cart
+          </button>
         </div>
       </div>
     </div>
