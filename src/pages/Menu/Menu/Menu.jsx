755910d24511749fded8464b99950f48498ce9cd
imransarkerweb <imransarker.web@gmail.com>
2023-05-30 09:29:51 +0600

    menu section added dynamically

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,8 +1,20 @@
 import Cover from "../../Shared/Cover/Cover";
 import imgMenu from "../../../assets/menu/banner3.jpg";
+import dessertImg from "../../../assets/menu/dessert-bg.jpeg";
+import pizzaImg from "../../../assets/menu/pizza-bg.jpg";
+import soupImg from "../../../assets/menu/soup-bg.jpg";
+import saladImg from "../../../assets/menu/salad-bg.jpg";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import useMenu from "../../../hooks/useMenu";
+import MenuCategory from "../MenuCategory/MenuCategory";
 
 const Menu = () => {
+  const [menu] = useMenu();
+  const desserts = menu.filter((item) => item.category === "dessert");
+  const soups = menu.filter((item) => item.category === "soup");
+  const pizzas = menu.filter((item) => item.category === "pizza");
+  const offereds = menu.filter((item) => item.category === "offered");
+  const salads = menu.filter((item) => item.category === "salad");
   return (
     <div>
       <Cover image={imgMenu} heading={"our menu"}></Cover>
@@ -10,6 +22,23 @@ const Menu = () => {
         subHeading={"---Don't miss---"}
         heading={"TODAY's Offer"}
       ></SectionTitle>
+      <MenuCategory items={offereds}></MenuCategory>
+      <MenuCategory
+        items={desserts}
+        title={"Desserts"}
+        img={dessertImg}
+      ></MenuCategory>
+      <MenuCategory
+        items={pizzas}
+        title={"Pizzas"}
+        img={pizzaImg}
+      ></MenuCategory>
+      <MenuCategory
+        items={salads}
+        title={"Salads"}
+        img={saladImg}
+      ></MenuCategory>
+      <MenuCategory items={soups} title={"Soups"} img={soupImg}></MenuCategory>
     </div>
   );
 };
--- src/pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,5 +1,18 @@
-const MenuCategory = () => {
-  return <div></div>;
+import Cover from "../../Shared/Cover/Cover";
+import MenuItem from "../../Shared/MenuItem/MenuItem";
+
+const MenuCategory = ({ items, title, img }) => {
+  return (
+    <>
+      {" "}
+      {title && <Cover image={img} heading={title}></Cover>}
+      <div className="grid md:grid-cols-2 gap-10 my-16">
+        {items.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default MenuCategory;
